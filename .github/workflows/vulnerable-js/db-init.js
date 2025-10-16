ba196b5d1d1e0619dc1b362591e9b674b83e69b1
James Brotsos <j.brotsos@gmail.com>
2025-10-16 16:02:44 -0700

    Delete .github/workflows/vulnerable-js directory

--- .github/workflows/vulnerable-js/db-init.js
+++
@@ -1,8 +0,0 @@
--- Simple sqlite users table
-CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT,
-  password TEXT
-);
-
-INSERT INTO users(username, password) VALUES ('alice', 'password123'), ('bob', 'hunter2');
